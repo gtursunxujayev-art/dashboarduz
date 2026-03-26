1758b51a226da37e3637bac59c5757e9bdae66f5
najotnurtheg <gtursunxujayev@gmail.com>
2026-03-26 11:10:32 +0500

    Create page.tsx

---
+++ apps/web/src/app/dashboard/course-sales/online/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import CourseTypeSalesView from '@/components/dashboard/course-sales/category-sales-view';
+
+export default function OnlineCourseSalesPage() {
+  return (
+    <CourseTypeSalesView
+      category="online"
+      title="Online sotuvi"
+      description="Online kurslari bo'yicha joriy sotuvlar, to'lov va qarzdorlik holati."
+    />
+  );
+}
