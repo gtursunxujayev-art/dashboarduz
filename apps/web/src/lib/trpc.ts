aba9eb769c683cac45f4dd5fc6b2c5067786c68a
najotnurtheg <gtursunxujayev@gmail.com>
2026-04-02 14:15:06 +0500

    Update trpc.ts

--- apps/web/src/lib/trpc.ts
+++ apps/web/src/lib/trpc.ts
@@ -1,11 +1,9 @@
-import { createTRPCReact } from '@trpc/react-query';
+import { createTRPCReact, type CreateTRPCReact } from '@trpc/react-query';
 import { httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
+import type { AppRouter } from '@dashboarduz/api-types';
 
-// Use any type for now to get the build to pass
-type AppRouter = any;
-
-export const trpc = createTRPCReact<AppRouter>() as any;
+export const trpc: CreateTRPCReact<AppRouter, unknown, null> = createTRPCReact<AppRouter>();
 
 function normalizeBaseUrl(url?: string) {
   return (url || 'http://localhost:3001').replace(/\/+$/, '');
